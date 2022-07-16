4d584159fc5c9cbb961d24c648243bb5c3499d50
NexyFC <91752683+NexyFC@users.noreply.github.com>
2022-07-16 15:43:49 +0100

    Pushing

--- Ball Roll/Assets/DiceHighlighting.cs
+++ Ball Roll/Assets/DiceHighlighting.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public class DiceHighlighting : MonoBehaviour
 {
-    public Sprite m_six;
-    public Sprite m_five;
-    public Sprite m_four;
-    public Sprite m_three;
-    public Sprite m_two;
-    public Sprite m_one;
-
-    private Sprite presetSprite;
-
-    public SpriteRenderer sp;
-
-    // Update is called once per frame
-    void Update()
-    {
-        presetSprite = sp.sprite;
-    }
+    
 }
--- Ball Roll/Assets/Scripts/PlayerMovement.cs
+++ Ball Roll/Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,5 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private int WhichRotationX=0;
-    private int WhichRotationY = 0;
-    private int WhichRRotationZ = 0;
 
-    public void RotateDown()
-    {
-        transform.Rotate(new Vector3(90f, 0f, 0f));
-        WhichRotationX++;
-        Debug.Log(WhichRotationX);
-        switch (WhichRotationX)
-        {
-            case 1:
-                transform.position += new Vector3(0f, -0.5f, 0f);
-                break;
-            case 2:
-                break;
-            case 3:
-                transform.position += new Vector3(0f, 1f, 0f);
-                break;
-            case 4:
-                transform.position += new Vector3(0f, -0.5f, 0f);
-                WhichRotationX = 0;
-                break;
-        }
-    }
 }
