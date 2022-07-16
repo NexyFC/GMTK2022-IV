0005d22ff449b18d0d07de1a43f6c7d075f2d857
Demqn0 <109368063+Demqn0@users.noreply.github.com>
2022-07-16 15:31:44 -0400

    i am good game dev

---
+++ Ball Roll/Assets/mainMenu.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resDropdown.ClearOptions();
+
+        List<string> options = new List <string>();
+        int currentResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resDropdown.AddOptions(options);
+        resDropdown.value = currentResIndex;
+        resDropdown.RefreshShownValue();
+    }
+
+    public void setRes(int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
+    public void setFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
