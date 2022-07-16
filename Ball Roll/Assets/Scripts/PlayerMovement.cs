061b9d4c554b0d95d657bd6c104744b0c7262f8a
NexyFC <91752683+NexyFC@users.noreply.github.com>
2022-07-16 19:15:49 +0100

    Movement

--- Ball Roll/Assets/Scripts/PlayerMovement.cs
+++ Ball Roll/Assets/Scripts/PlayerMovement.cs
@@ -4,4 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Quaternion currentRotation = transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(-180, 0, 0);
+            transform.localRotation = Quaternion.RotateTowards(currentRotation, wantedRotation, 10000f * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Quaternion currentRotation = transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(180, 0, 0);
+            transform.localRotation = Quaternion.RotateTowards(currentRotation, wantedRotation, 10000f * Time.deltaTime);
+        }
+    }
 }
